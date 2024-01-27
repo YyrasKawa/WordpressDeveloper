181c28a64a9b9314958020a5412bab7bdfd881ec
Yury Kavalenka <yurykavalenka@gmail.com>
2024-01-27 18:18:34 +0100

    PHP function

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-git
+<h1><?php bloginfo('name'); ?></h1>
+<h2><?php bloginfo('description');  ?></h2>
+
+<?php
+function myFirstFunction($name, $color) {
+
+    echo "<p>Hi, my name is $name and my favorite color is $color</p>";
+};
+
+myFirstFunction('John', 'white');
+myFirstFunction('Jane', 'blue');
+
+?>
